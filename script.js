4384f751a7cadd7ee871d403e506b8c4d5e0efe0
h3cat3 <dumitrescu.c.e@gmail.com>
2026-01-01 18:30:08 +0000

    menu and reset
    create the menu for small screens
    and add css  reset

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainImage = document.getElementById('main-image');
     const thumbnails = document.querySelectorAll('.thumb');
 
+    //open menu for small screens
+    const openMenu = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+    const closeBtn = document.getElementById('close');
+    openMenu.addEventListener('click', () => {
+        menu.classList.add('menu-mobile');
+        menu.classList.remove('menu');
+        closeBtn.style.display = 'block';
+        closeBtn.addEventListener('click', () => {
+            menu.classList.remove('menu-mobile');
+            menu.classList.add('menu');
+            closeBtn.style.display = 'none';
+        })
+    })
     //transforming the main image
     thumbnails.forEach(thumb => {
         thumb.addEventListener('click', function() {
