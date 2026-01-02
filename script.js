989add1da98aa37081f7cec91345602faba7b248
h3cat3 <dumitrescu.c.e@gmail.com>
2026-01-02 16:49:48 +0000

    gallery
    display images  for small screen, for big screen with a thumbnail and as a modal

--- script.js
+++ script.js
@@ -16,12 +16,62 @@ document.addEventListener('DOMContentLoaded', () => {
             closeBtn.style.display = 'none';
         })
     })
+    
+    function changeImage(thumb) {
+         mainImage.src = thumb.src;
+    }
     //transforming the main image
     thumbnails.forEach(thumb => {
         thumb.addEventListener('click', function() {
-            const largeImageSrc = this.dataset.large;
-            mainImage.src = thumb.src;
-            console.log(largeImageSrc);
+           changeImage(thumb);
+            
         })
     })
+
+    //arrow buttons
+    const previewBtn = document.getElementById('preview');
+    const nextBtn = document.getElementById('next');
+    let index = 0;
+    previewBtn.addEventListener('click', () => {
+        if (index > 0) {
+            index -= 1;
+            changeImage(thumbnails[index]);
+        }
+    })
+    nextBtn.addEventListener('click', () => {
+        if (index < thumbnails.length) {
+            index += 1;
+            changeImage(thumbnails[index]);
+        }
+    })
+
+    //make modal gallery
+    const modal = document.getElementById('gallery');
+    const modalContent =document.getElementById('modal');
+    const galleryClose =  document.getElementById('close-gallery');
+    mainImage.addEventListener('click', () => {
+        if (window.innerWidth >= 1024) {
+            modal.classList.add('modal');
+            modalContent.classList.add('modal-content')
+            galleryClose.style.display= 'block';
+            galleryClose.style.paddingBottom = '1rem';
+            galleryClose.style.marginLeft = 'auto'
+            previewBtn.style.display = 'block';
+            previewBtn.style.marginRight = '-1.5rem';
+            nextBtn.style.display = 'block';
+            nextBtn.style.marginLeft = '-1.5rem';
+            galleryClose.addEventListener('click', () => {
+                modal.classList.remove('modal');
+                modalContent.classList.remove('modal-content');
+                galleryClose.style.display = 'none'
+                previewBtn.style.display = 'none';
+                
+                nextBtn.style.display = 'none';
+
+            })
+
+        }
+        
+    })
+
 })
